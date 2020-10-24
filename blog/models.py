2fc3f3ee781f9cebcd12f7237b6c41178ac84cbf
orlan-mirkovic <orlan.mirkovic@gmail.com>
2020-10-25 00:41:55 +0200

    I made this blog by myself.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    date = models.DateField()
+    title = models.CharField(max_length=40)
+    text = models.CharField(max_length=200)
