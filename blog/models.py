e37d0b3f2deb69420f01d0d5a4311cd58e4e2141
orlan-mirkovic <orlan.mirkovic@gmail.com>
2020-10-28 22:07:36 +0100

    Coments about __str__ in models.py

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=40)
     body = models.TextField()
 
+    # this will display on admin page summary instead of Blog object (4)
     def __str__(self):
         return self.title
 
--- jobs/models.py
+++ jobs/models.py
@@ -7,5 +7,6 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
+    # this will display on admin page summary instead of Job object (4)
     def __str__(self):
         return self.summary
